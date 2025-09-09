9d3af10dace144ac029a4925049073cff3002253
danielgp <danielgarduno282@gmail.com>
2025-09-09 11:09:05 -0600

    se resolvieron los tasks 3 y 4

--- task3.js
+++ task3.js
@@ -1,2 +1,37 @@
 // Task 3: addUser(first_name, last_name, email)
+async function addUser(firstName, lastName, email) {
+  try {
+    const response = await fetch(`http://localhost:3000/users`);
+    const users = await response.json();
+    const nuevoId = users.length+ 1;
+
+    const newUser = {
+      id: nuevoId,
+      firstname: firstName,
+      lastname: lastName,
+      emailusuario: email,
+    };
+
+    const postResponse = await fetch(`http://localhost:3000/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    });
+
+    if (!postResponse.ok) {
+      throw new Error(`Error al agregar usuario: ${postResponse.statusText}`);
+    }
+
+    const addUsuario = await postResponse.json();
+    console.log('Usuario agregado exitosamente:', addUsuario);
+
+  } catch (error) {
+    console.error('No se pudo agregar el usuario:', error);
+  }
+}
+
+export { addUser };
+
 
--- task4.js
+++ task4.js
@@ -1,2 +1,17 @@
 // Task 4: delUser(number)
+async function delUser(id) {
+  try {
+    const response = await fetch(`http://localhost:3000/users/${id}`, {
+      method: 'DELETE',
+    });
 
+    if (!response.ok) {
+      throw new Error(`Error al eliminar usuario: ${response.statusText}`);
+    }
+
+    console.log(`Usuario con ID ${id} eliminado exitosamente.`);
+  } catch (error) {
+    console.error('No se pudo eliminar el usuario:', error);
+  }
+}
+export {delUser};
