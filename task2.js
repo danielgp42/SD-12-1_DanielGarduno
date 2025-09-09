00827dfc4e686f286a08d587d0b5bf5df965fb1b
danielgp <danielgarduno282@gmail.com>
2025-09-09 11:01:09 -0600

    se realizó task2

--- task2.js
+++ task2.js
@@ -1,2 +1,16 @@
 // Task 2: listUsers()
 
+async function listUsers() {
+  try {
+    const response = await fetch(`http://localhost:3000/users`);
+    if (!response.ok) {
+      throw new Error(`Existe un error en la solicitud: ${response.status}`);
+    }
+    const users = await response.json();
+    console.log('Lista de usuarios:', users);
+  } catch (error) {
+    console.error('No se pudo obtener la lista de usuarios:', error);
+  }
+}
+
+export { listUsers };
